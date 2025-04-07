7c1417e9e6db9fd62ed3a82a39afa10111b5c5c9
phil <uniphil@gmail.com>
2025-04-06 21:11:44 -0400

    forgot to add storage.rs now

---
+++ ufos/src/storage.rs
@@ -0,0 +1,18 @@
+use crate::{error::StorageError, Cursor, EventBatch};
+use std::path::Path;
+
+pub trait StorageWhatever<R: StoreReader, W: StoreWriter, C> { // TODO: extract this
+    fn init(
+        path: impl AsRef<Path>,
+        endpoint: String,
+        force_endpoint: bool,
+        config: C,
+    ) -> Result<(R, W, Option<Cursor>), StorageError> where Self: Sized;
+}
+
+pub trait StoreWriter {
+    fn insert_batch(&mut self, event_batch: EventBatch) -> Result<(), StorageError>;
+}
+
+pub trait StoreReader: Clone {
+}
