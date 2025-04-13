02732001cb968e239353fda8c87a9e25c30c4143
phil <uniphil@gmail.com>
2025-04-13 15:27:43 -0400

    more minimal fuzz repro

--- ufos/fuzz/fuzz_targets/cardinality_estimator.rs
+++ ufos/fuzz/fuzz_targets/cardinality_estimator.rs
@@ -1,30 +1,20 @@
 #![no_main]
 
-use jetstream::exports::Did;
-use bincode::config::{Configuration, BigEndian, Fixint, Limit, standard};
+use bincode::config::{Configuration, LittleEndian, Varint, Limit, standard};
 use bincode::serde::decode_from_slice;
 use cardinality_estimator::CardinalityEstimator;
 use libfuzzer_sys::fuzz_target;
 
-#[cfg(not(target_env = "msvc"))]
-use tikv_jemallocator::Jemalloc;
-
-#[cfg(not(target_env = "msvc"))]
-#[global_allocator]
-static GLOBAL: Jemalloc = Jemalloc;
-
-type C = Configuration<BigEndian, Fixint, Limit<1048576>>;
-static BINCODE_CONF: C =
-    standard()
-        .with_big_endian()
-        .with_fixed_int_encoding()
-        .with_limit::<1048576>();
+type C = Configuration<LittleEndian, Varint, Limit<1048576>>;
+static BINCODE_CONF: C = standard().with_limit::<1048576>();
 
 fuzz_target!(|data: &[u8]| {
-    if let Ok((estimator, _n)) = decode_from_slice::<CardinalityEstimator<Did>, C>(
+    if let Ok((mut estimator, _n)) = decode_from_slice::<CardinalityEstimator<String>, C>(
         data,
         BINCODE_CONF,
     ) {
-        estimator.estimate();
+        // crash happens *much* faster if we just do kinda anything with the estimator
+        estimator.insert(&"asdf".to_string());
+        assert!(estimator.estimate() > 0);
     }
 });
